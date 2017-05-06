e4e11e80b31c49dc21ab2ce47dae71676ecd2b4a
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-05-06 18:27:06 +0800

    Tachyon Unit script update

--- Anime/c511002571.lua
+++ Anime/c511002571.lua
@@ -10,19 +10,35 @@ function c511002571.initial_effect(c)
 	e1:SetTarget(c511002571.target)
 	e1:SetOperation(c511002571.activate)
 	c:RegisterEffect(e1)
+	if not c511002571.global_check then
+		c511002571.global_check=true
+		local ge2=Effect.CreateEffect(c)
+		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge2:SetCode(EVENT_ADJUST)
+		ge2:SetCountLimit(1)
+		ge2:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
+		ge2:SetOperation(c511002571.archchk)
+		Duel.RegisterEffect(ge2,0)
+	end
+end
+function c511002571.archchk(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(0,420)==0 then 
+		Duel.CreateToken(tp,420)
+		Duel.CreateToken(1-tp,420)
+		Duel.RegisterFlagEffect(0,420,0,0,0)
+	end
 end
 function c511002571.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.CheckLPCost(tp,500) end
 	Duel.PayLPCost(tp,500)
 end
 function c511002571.filter(c,e,tp)
-	if not c:IsCode(88177324) and not c:IsCode(68396121) then return false end
-	if c:IsFacedown() or not c:IsHasEffect(511002571) then return false end
+	if c:IsFacedown() or not c420.IsTachyon(c) or not c:IsHasEffect(511002571) then return false end
 	local eff={c:GetCardEffect(511002571)}
 	for i=1,#eff do
 		local te=eff[i]:GetLabelObject()
 		local tg=te:GetTarget()
-		if not tg or tg(e,tp,Group.CreateGroup(),PLAYER_NONE,0,eff[i],REASON_EFFECT,PLAYER_NONE,0) then return true end
+		if not tg or tg(te,tp,Group.CreateGroup(),PLAYER_NONE,0,eff[i],REASON_EFFECT,PLAYER_NONE,0) then return true end
 	end
 	return false
 end
@@ -42,7 +58,7 @@ function c511002571.activate(e,tp,eg,ep,ev,re,r,rp)
 		for i=1,#eff do
 			local temp=eff[i]:GetLabelObject()
 			local tg=temp:GetTarget()
-			if not tg or tg(e,tp,Group.CreateGroup(),PLAYER_NONE,0,eff[i],REASON_EFFECT,PLAYER_NONE,0) then
+			if not tg or tg(temp,tp,Group.CreateGroup(),PLAYER_NONE,0,eff[i],REASON_EFFECT,PLAYER_NONE,0) then
 				table.insert(ac,temp)
 				table.insert(acd,temp:GetDescription())
 			end
@@ -56,7 +72,7 @@ function c511002571.activate(e,tp,eg,ep,ev,re,r,rp)
 		if not te then return end
 		local tg=te:GetTarget()
 		local op=te:GetOperation()
-		if tg then tg(e,tp,Group.CreateGroup(),PLAYER_NONE,0,eff[1],REASON_EFFECT,PLAYER_NONE,1) end
+		if tg then tg(te,tp,Group.CreateGroup(),PLAYER_NONE,0,eff[1],REASON_EFFECT,PLAYER_NONE,1) end
 		Duel.BreakEffect()
 		tc:CreateEffectRelation(e)
 		Duel.BreakEffect()
@@ -68,7 +84,7 @@ function c511002571.activate(e,tp,eg,ep,ev,re,r,rp)
 				etc=g:GetNext()
 			end
 		end
-		if op then op(e,tp,Group.CreateGroup(),PLAYER_NONE,0,eff[1],REASON_EFFECT,PLAYER_NONE,1) end
+		if op then op(te,tp,Group.CreateGroup(),PLAYER_NONE,0,eff[1],REASON_EFFECT,PLAYER_NONE,1) end
 		tc:ReleaseEffectRelation(e)
 		if etc then	
 			etc=g:GetFirst()
