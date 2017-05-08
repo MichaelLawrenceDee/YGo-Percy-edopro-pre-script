1ec600d770c88159059d6190e8791da6b1a21474
edo9300 <edoardo762@gmail.com>
2017-05-08 19:01:14 +0200

    Check for already existing code for the listed materials

--- utility.lua
+++ utility.lua
@@ -459,12 +459,18 @@ function Auxiliary.AddFusionProcMix(c,sub,insf,...)
 		if type(val[i])=='function' then
 			fun[i]=function(c) return val[i](c) and not c:IsHasEffect(6205579) end
 		else
+			local addmat=true
 			if sub then
 				fun[i]=function(c,fc,sub) return c:IsFusionCode(val[i]) or (sub and c:CheckFusionSubstitute(fc)) end
 			else
 				fun[i]=function(c) return c:IsFusionCode(val[i]) end
 			end
-			table.insert(mat,val[i])
+			for index, value in ipairs(mat) do
+				if value==val[i] then
+					addmat=false
+				end
+			end
+			if addmat then table.insert(mat,val[i]) end
 		end
 	end
 	if #mat>0 and c.material_count==nil then
@@ -577,12 +583,18 @@ function Auxiliary.AddFusionProcMixRep(c,sub,insf,fun1,minc,maxc,...)
 		if type(val[i])=='function' then
 			fun[i]=function(c) return val[i](c) and not c:IsHasEffect(6205579) end
 		else
+			local addmat=true
 			if sub then
 				fun[i]=function(c,fc,sub) return c:IsFusionCode(val[i]) or (sub and c:CheckFusionSubstitute(fc)) end
 			else
 				fun[i]=function(c) return c:IsFusionCode(val[i]) end
 			end
-			table.insert(mat,val[i])
+			for index, value in ipairs(mat) do
+				if value==val[i] then
+					addmat=false
+				end
+			end
+			if addmat then table.insert(mat,val[i]) end
 		end
 	end
 	if #mat>0 and c.material_count==nil then
@@ -763,12 +775,6 @@ function Auxiliary.AddFusionProcCodeRep(c,code1,cc,sub,insf)
 	local code={}
 	for i=1,cc do
 		code[i]=code1
-	end
-	if c.material_count==nil then
-		local code=c:GetOriginalCode()
-		local mt=_G["c" .. code]
-		mt.material_count=1
-		mt.material={code1}
 	end
 	Auxiliary.AddFusionProcMix(c,sub,insf,table.unpack(code))
 end
