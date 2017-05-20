a102a078014852cfbcc21a52325723dc5a64259b
edo9300 <edoardo762@gmail.com>
2017-05-20 16:13:20 +0200

    Updated fusionRep procedure and link summon

--- utility.lua
+++ utility.lua
@@ -1320,27 +1320,29 @@ function Auxiliary.FOperationMixRep(insf,sub,fun1,minc,maxc,...)
 				Duel.SetFusionMaterial(sg)
 			end
 end
-function Auxiliary.FCheckMixRep(c,sg,g,fc,sub,fun1,minc,maxc,fun2,...)
-	g:AddCard(c)
-	local res=false
+function Auxiliary.FCheckMixRep(sg,g,fc,sub,fun1,minc,maxc,fun2,...)
 	if fun2 then
-		if fun2(c,fc,sub) then
-			local sub=sub and fun2(c,fc,false)
-			res=sg:IsExists(Auxiliary.FCheckMixRep,1,g,sg,g,fc,sub,fun1,minc,maxc,...)
-		end
-	elseif fun1(c,fc,sub) then
-		local sub=sub and fun1(c,fc,false)
+		return sg:IsExists(Auxiliary.FCheckMixRepFilter,1,g,sg,g,fc,sub,fun1,minc,maxc,fun2,...)
+	else
 		local ct1=sg:FilterCount(fun1,g,fc,sub)
 		local ct2=sg:FilterCount(fun1,g,fc,false)
-		res=ct1==sg:GetCount()-g:GetCount() and (not sub or ct1-ct2<=1)
+		return ct1==sg:GetCount()-g:GetCount() and ct1-ct2<=1
 	end
-	g:RemoveCard(c)
-	return res
+end
+function Auxiliary.FCheckMixRepFilter(c,sg,g,fc,sub,fun1,minc,maxc,fun2,...)
+	if fun2(c,fc,sub) then
+		g:AddCard(c)
+		local sub=sub and fun2(c,fc,false)
+		local res=Auxiliary.FCheckMixRep(sg,g,fc,sub,fun1,minc,maxc,...)
+		g:RemoveCard(c)
+		return res
+	end
+	return false
 end
 function Auxiliary.FCheckMixRepGoal(tp,sg,fc,sub,fun1,minc,maxc,...)
 	if sg:GetCount()<minc+#{...} or sg:GetCount()>maxc+#{...} then return false end
 	local g=Group.CreateGroup()
-	return sg:IsExists(Auxiliary.FCheckMixRep,1,nil,sg,g,fc,sub,fun1,minc,maxc,...) and Duel.GetLocationCountFromEx(tp,tp,sg,fc)>0
+	return Auxiliary.FCheckMixRep(sg,g,fc,sub,fun1,minc,maxc,...) and Duel.GetLocationCountFromEx(tp,tp,sg,fc)>0
 		and (not Auxiliary.FCheckAdditional or Auxiliary.FCheckAdditional(tp,sg,fc))
 end
 function Auxiliary.FCheckMixRepTemplate(c,cond,tp,mg,sg,g,fc,sub,fun1,minc,maxc,...)
@@ -1394,13 +1396,14 @@ function Auxiliary.FCheckSelectMixRepAll(c,tp,mg,sg,g,fc,sub,fun1,minc,maxc,fun2
 			g:RemoveCard(c)
 			return res
 		end
-	elseif fun1(c,fc,sub) then
+	elseif maxc>0 and fun1(c,fc,sub) then
 		g:AddCard(c)
 		local sub=sub and fun1(c,fc,false)
 		local res=Auxiliary.FCheckSelectMixRep(tp,mg,sg,g,fc,sub,fun1,minc-1,maxc-1)
 		g:RemoveCard(c)
 		return res
 	end
+	return false
 end
 function Auxiliary.FCheckSelectMixRepM(c,tp,...)
 	return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE)
@@ -1957,24 +1960,21 @@ function Auxiliary.LinkTarget(f,minc,maxc)
 	return	function(e,tp,eg,ep,ev,re,r,rp,chk,c)
 				local mg=Duel.GetMatchingGroup(Auxiliary.LConditionFilter,tp,LOCATION_MZONE,0,nil,f)
 				local sg=Group.CreateGroup()
-				local i=0
 				local cancel=false
-				while i<maxc do
-					local cg=mg:Filter(Auxiliary.LCheckRecursive,sg,tp,sg,mg,c,i,minc,maxc)
+				while sg:GetCount()<maxc do
+					local cg=mg:Filter(Auxiliary.LCheckRecursive,sg,tp,sg,mg,c,sg:GetCount(),minc,maxc)
 					if cg:GetCount()==0 then break end
-					if i>minc and i<=maxc and Auxiliary.LCheckGoal(tp,sg,c,minc,i) then
+					if sg:GetCount()>minc and sg:GetCount()<=maxc and Auxiliary.LCheckGoal(tp,sg,c,minc,sg:GetCount()) then
 						cancel=true 
 					else 
 						cancel=false
 					end
-					local tc=Group.SelectUnselect(cg,sg,tp,cancel,i==0 or cancel,1,1)
+					local tc=Group.SelectUnselect(cg,sg,tp,cancel,sg:GetCount()==0 or cancel,1,1)
 					if not tc then break end
 					if not sg:IsContains(tc) then
 						sg:AddCard(tc)
-						i=i+1
 					else
 						sg:RemoveCard(tc)
-						i=i-1
 					end
 				end
 				if sg:GetCount()>0 then
