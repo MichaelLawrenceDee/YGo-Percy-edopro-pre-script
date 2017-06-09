f7cf784b5eb2002fefd04bb85ad3374c83f996e3
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-06-09 14:55:11 +0800

    Harmonic Synchro Fusion Final Fix

--- Anime/c511002620.lua
+++ Anime/c511002620.lua
@@ -24,25 +24,25 @@ function c511002620.filterchk(c,g,sg,e,tp,fc,sc)
 	sg:AddCard(c)
 	local res
 	if sg:GetCount()<fc.min_material_count then
-		res=sg:IsExists(c511002620.filterchk,1,sg,g,sg,e,tp)
+		res=g:IsExists(c511002620.filterchk,1,sg,g,sg,e,tp,fc,sc)
 	elseif sg:GetCount()<fc.max_material_count then
-		res=c511002620.matchk(fc,sc,sg,tp)
-			or sg:IsExists(c511002620.filterchk,1,sg,g,sg,e,tp)
+		res=c511002620.matchk(fc,sc,sg,e,tp)
+			or g:IsExists(c511002620.filterchk,1,sg,g,sg,e,tp,fc,sc)
 	else
-		res=c511002620.matchk(fc,sc,sg,tp)
+		res=c511002620.matchk(fc,sc,sg,e,tp)
 	end
 	sg:RemoveCard(c)
 	return res
 end
-function c511002620.matchk(fc,sc,sg,tp)
+function c511002620.matchk(fc,sc,sg,e,tp)
 	local t={}
 	local tc=sg:GetFirst()
 	while tc do
-		local e1=Effect.CreateEffect(e:GetHandler())
+		local e1=Effect.CreateEffect(tc)
 		e1:SetType(EFFECT_TYPE_FIELD)
 		e1:SetRange(LOCATION_MZONE)
 		e1:SetCode(EFFECT_MUST_BE_SMATERIAL)
-		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE)
 		e1:SetTargetRange(1,0)
 		e1:SetReset(RESET_CHAIN)
 		tc:RegisterEffect(e1)
@@ -74,11 +74,11 @@ function c511002620.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(c511002620.filter,tp,LOCATION_MZONE,0,nil,e,fc,sc)
 	local mat=Group.CreateGroup()
 	while mat:GetCount()<fc.max_material_count do
-		local cancel=mat:GetCount()>0 and c511002620.matchk(fc,sc,mat,tp)
-		local g=cg:Filter(c511002620.filterchk,mat,g,mat,e,tp,fc,sc)
-		if g:GetCount()<=0 then break end
+		local cancel=mat:GetCount()>0 and c511002620.matchk(fc,sc,mat,e,tp)
+		local sg=g:Filter(c511002620.filterchk,mat,g,mat,e,tp,fc,sc)
+		if sg:GetCount()<=0 then break end
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FMATERIAL)
-		local tc=Group.SelectUnselect(g,mat,tp,cancel,cancel)
+		local tc=Group.SelectUnselect(sg,mat,tp,cancel,cancel)
 		if not tc then break end
 		if mat:IsContains(tc) then
 			mat:RemoveCard(tc)
