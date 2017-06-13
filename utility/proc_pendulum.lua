a6bc7e47591b3662efcec249be060337f4ed576a
edo9300 <edoardo762@gmail.com>
2017-06-13 19:01:06 +0200

    Utility split up

---
+++ utility/proc_pendulum.lua
@@ -0,0 +1,121 @@
+
+--add procedure to Pendulum monster, also allows registeration of activation effect
+function Auxiliary.EnablePendulumAttribute(c,reg)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(1163)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC_G)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1,10000000)
+	e1:SetCondition(Auxiliary.PendCondition())
+	e1:SetOperation(Auxiliary.PendOperation())
+	e1:SetValue(SUMMON_TYPE_PENDULUM)
+	c:RegisterEffect(e1)
+	--register by default
+	if reg==nil or reg then
+		local e2=Effect.CreateEffect(c)
+		e2:SetDescription(1160)
+		e2:SetType(EFFECT_TYPE_ACTIVATE)
+		e2:SetCode(EVENT_FREE_CHAIN)
+		e2:SetRange(LOCATION_HAND)
+		c:RegisterEffect(e2)
+	end
+end
+function Auxiliary.PConditionFilter(c,e,tp,lscale,rscale)
+	local lv=0
+	if c.pendulum_level then
+		lv=c.pendulum_level
+	else
+		lv=c:GetLevel()
+	end
+	return (c:IsLocation(LOCATION_HAND) or (c:IsFaceup() and c:IsType(TYPE_PENDULUM)))
+		and ((lv>lscale and lv<rscale) or c:IsHasEffect(511004423)) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_PENDULUM,tp,false,false)
+		and not c:IsForbidden()
+end
+function Auxiliary.PendCondition()
+	return	function(e,c,og)
+				if c==nil then return true end
+				local tp=c:GetControler()
+				local rpz=Duel.GetFieldCard(tp,LOCATION_PZONE,1)
+				if rpz==nil or c==rpz then return false end
+				local lscale=c:GetLeftScale()
+				local rscale=rpz:GetRightScale()
+				if lscale>rscale then lscale,rscale=rscale,lscale end
+				local loc=0
+				if Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then loc=loc+LOCATION_HAND end
+				if Duel.GetLocationCountFromEx(tp)>0 then loc=loc+LOCATION_EXTRA end
+				if loc==0 then return false end
+				local g=nil
+				if og then
+					g=og:Filter(Card.IsLocation,nil,loc)
+				else
+					g=Duel.GetFieldGroup(tp,loc,0)
+				end
+				return g:IsExists(Auxiliary.PConditionFilter,1,nil,e,tp,lscale,rscale)
+			end
+end
+function Auxiliary.PendOperation()
+	return	function(e,tp,eg,ep,ev,re,r,rp,c,sg,og)
+				local rpz=Duel.GetFieldCard(tp,LOCATION_PZONE,1)
+				local lscale=c:GetLeftScale()
+				local rscale=rpz:GetRightScale()
+				if lscale>rscale then lscale,rscale=rscale,lscale end
+				local ft1=Duel.GetLocationCount(tp,LOCATION_MZONE)
+				local ft2=Duel.GetLocationCountFromEx(tp)
+				local ft=Duel.GetUsableMZoneCount(tp)
+				if Duel.IsPlayerAffectedByEffect(tp,59822133) then
+					if ft1>0 then ft1=1 end
+					if ft2>0 then ft2=1 end
+					ft=1
+				end
+				local loc=0
+				if ft1>0 then loc=loc+LOCATION_HAND end
+				if ft2>0 then loc=loc+LOCATION_EXTRA end
+				local tg=nil
+				if og then
+					tg=og:Filter(Card.IsLocation,nil,loc):Filter(Auxiliary.PConditionFilter,nil,e,tp,lscale,rscale)
+				else
+					tg=Duel.GetMatchingGroup(Auxiliary.PConditionFilter,tp,loc,0,nil,e,tp,lscale,rscale)
+				end
+				ft1=math.min(ft1,tg:FilterCount(Card.IsLocation,nil,LOCATION_HAND))
+				ft2=math.min(ft2,tg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA))
+				local ect=c29724053 and Duel.IsPlayerAffectedByEffect(tp,29724053) and c29724053[tp]
+				if ect and ect<ft2 then ft2=ect end
+				while true do
+					local ct1=tg:FilterCount(Card.IsLocation,nil,LOCATION_HAND)
+					local ct2=tg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA)
+					local ct=ft
+					if ct1>ft1 then ct=math.min(ct,ft1) end
+					if ct2>ft2 then ct=math.min(ct,ft2) end
+					if ct<=0 then break end
+					if sg:GetCount()>0 and not Duel.SelectYesNo(tp,210) then ft=0 break end
+					Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+					local g=tg:Select(tp,1,ct,nil)
+					tg:Sub(g)
+					sg:Merge(g)
+					if g:GetCount()<ct then ft=0 break end
+					ft=ft-g:GetCount()
+					ft1=ft1-g:FilterCount(Card.IsLocation,nil,LOCATION_HAND)
+					ft2=ft2-g:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA)
+				end
+				if ft>0 then
+					local tg1=tg:Filter(Card.IsLocation,nil,LOCATION_HAND)
+					local tg2=tg:Filter(Card.IsLocation,nil,LOCATION_EXTRA)
+					if ft1>0 and ft2==0 and tg1:GetCount()>0 and (sg:GetCount()==0 or Duel.SelectYesNo(tp,210)) then
+						local ct=math.min(ft1,ft)
+						Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+						local g=tg1:Select(tp,1,ct,nil)
+						sg:Merge(g)
+					end
+					if ft1==0 and ft2>0 and tg2:GetCount()>0 and (sg:GetCount()==0 or Duel.SelectYesNo(tp,210)) then
+						local ct=math.min(ft2,ft)
+						Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+						local g=tg2:Select(tp,1,ct,nil)
+						sg:Merge(g)
+					end
+				end
+				Duel.HintSelection(Group.FromCards(c))
+				Duel.HintSelection(Group.FromCards(rpz))
+			end
+end
