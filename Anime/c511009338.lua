2ca6ff24a281d2168a945f57ece414dbf78f30fc
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-06-22 10:24:13 +0800

    Forbidden Beast, Kabuki Stage, Parasite

--- Anime/c511009338.lua
+++ Anime/c511009338.lua
@@ -10,13 +10,26 @@ function c511009338.initial_effect(c)
 	e1:SetTarget(c511009338.target)
 	e1:SetOperation(c511009338.activate)
 	c:RegisterEffect(e1)
+	if not c511009338.global_check then
+		c511009338.global_check=true
+		local ge2=Effect.CreateEffect(c)
+		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		ge2:SetCode(EVENT_ADJUST)
+		ge2:SetCountLimit(1)
+		ge2:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
+		ge2:SetOperation(c511009338.archchk)
+		Duel.RegisterEffect(ge2,0)
+	end
 end
---OCG Parasite collection
-c511009338.collection={
-	[6205579]=true;
-}
+function c511009338.archchk(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetFlagEffect(0,420)==0 then 
+		Duel.CreateToken(tp,420)
+		Duel.CreateToken(1-tp,420)
+		Duel.RegisterFlagEffect(0,420,0,0,0)
+	end
+end
 function c511009338.cfilter(c)
-	return c:IsFaceup() and (c511009338.collection[c:GetCode()] or c:IsSetCard(0x410))
+	return c:IsFaceup() and c:IsParasite()
 end
 function c511009338.condition(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.IsExistingMatchingCard(c511009338.cfilter,tp,LOCATION_MZONE,0,1,nil)
@@ -26,14 +39,7 @@ function c511009338.acfilter(c,e,tp,eg,ep,ev,re,r,rp,chain)
 	if not te then return false end
 	if not c:IsType(TYPE_SPELL) or not c:IsSSetable(true) then return false end
 	if not c:IsType(TYPE_FIELD) and Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return false end
-	if c:IsHasEffect(EFFECT_CANNOT_TRIGGER) then return false end
-	local pre={Duel.GetPlayerEffect(tp,EFFECT_CANNOT_ACTIVATE)}
-	if pre[1] then
-		for i,eff in ipairs(pre) do
-			local prev=eff:GetValue()
-			if type(prev)~='function' or prev(eff,te,tp) then return false end
-		end
-	end
+	if te:IsActivatable(tp) then return true end
 	local condition=te:GetCondition()
 	local cost=te:GetCost()
 	local target=te:GetTarget()
@@ -81,14 +87,7 @@ function c511009338.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.SSet(tp,tc)
 		if not tc:IsLocation(LOCATION_SZONE) then return end
 		local te=tc:GetActivateEffect()
-		if not te or tc:IsHasEffect(EFFECT_CANNOT_TRIGGER) then return end
-		local pre={Duel.GetPlayerEffect(tp,EFFECT_CANNOT_ACTIVATE)}
-		if pre[1] then
-			for i,eff in ipairs(pre) do
-				local prev=eff:GetValue()
-				if type(prev)~='function' or prev(eff,te,tp) then return end
-			end
-		end
+		if not te then return end
 		Duel.ChangePosition(tc,POS_FACEUP)
 		local tg=te:GetTarget()
 		local co=te:GetCost()
