2ef44935b7c02919293a0b0280f03d24a65f250b
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-07-31 22:51:10 +0800

    Proxy Dragon fix

--- Beta/c22862454.lua
+++ Beta/c22862454.lua
@@ -1,6 +1,4 @@
 --プロキシー・ドラゴン
---Proxy Dragon
---Script by nekrozar
 function c22862454.initial_effect(c)
 	--link summon
 	aux.AddLinkProcedure(c,nil,2,2)
@@ -19,18 +17,18 @@ function c22862454.repfilter(c,tp)
 	return c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD)
 		and c:IsReason(REASON_BATTLE+REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
 end
-function c22862454.desfilter(c,tp)
-	return c:IsControler(tp) and c:IsType(TYPE_MONSTER)
+function c22862454.desfilter(c,e,tp)
+	return c:IsControler(tp) and c:IsType(TYPE_MONSTER) and c:IsDestructable(e)
 		and not c:IsStatus(STATUS_DESTROY_CONFIRMED+STATUS_BATTLE_DESTROYED)
 end
 function c22862454.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
 	local g=c:GetLinkedGroup()
 	if chk==0 then return eg:IsExists(c22862454.repfilter,1,nil,tp)
-		and g:IsExists(c22862454.desfilter,1,nil,tp) end
-	if Duel.SelectYesNo(tp,aux.Stringid(22862454,0)) then
+		and g:IsExists(c22862454.desfilter,1,nil,e,tp) end
+	if Duel.SelectEffectYesNo(tp,c,96) then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
-		local sg=g:FilterSelect(tp,c22862454.desfilter,1,1,nil,tp)
+		local sg=g:FilterSelect(tp,c22862454.desfilter,1,1,nil,e,tp)
 		e:SetLabelObject(sg:GetFirst())
 		Duel.HintSelection(sg)
 		sg:GetFirst():SetStatus(STATUS_DESTROY_CONFIRMED,true)
