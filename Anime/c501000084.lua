34dfcc95797fd733d4fc9dac579053b7f21405a7
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-08-14 00:56:50 +0800

    Fix

--- Anime/c501000084.lua
+++ Anime/c501000084.lua
@@ -9,6 +9,13 @@ function c501000084.initial_effect(c)
 	e1:SetCode(EFFECT_MATCH_KILL)
 	e1:SetCondition(c501000084.con)
 	c:RegisterEffect(e1)
+	--spsummon condition
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e3:SetValue(c501000084.limit)
+	c:RegisterEffect(e3)
 end
 function c501000084.filter(c)
 	return c:IsLinkType(TYPE_EFFECT) and c:IsRace(RACE_MACHINE)
@@ -29,4 +36,7 @@ function c501000084.con(e)
 	local g3=lc3:GetMutualLinkedGroup()
 	if not g5 or not g1 or not g2 or not g3 or not g6 then return false end
 	return g5:IsContains(lc1) and g1:IsContains(lc2) and g2:IsContains(lc3) and g3:IsContains(lc6)
+end
+function c501000084.limit(e,se,sp,st)
+	return bit.band(st,SUMMON_TYPE_LINK)==SUMMON_TYPE_LINK
 end
