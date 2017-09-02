1a739a419f46c23221efdfe5fa15d8606b811a42
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-09-02 14:26:14 +0800

    Proc Fix

--- TCG-OCG/c61204971.lua
+++ TCG-OCG/c61204971.lua
@@ -2,7 +2,7 @@
 function c61204971.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	aux.AddFusionProcCode2(c,20721928,84327329,true,true)
+	aux.AddFusionProcMix(c,true,true,20721928,84327329)
 	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
