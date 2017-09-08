0d77cde553201fb0285b8d318f60123c97a8861c
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-09-08 13:46:13 +0800

    Redundant code

--- Anime/c511001822.lua
+++ Anime/c511001822.lua
@@ -34,7 +34,7 @@ end
 function c511001822.filter2(c,e,tp,mc,rk)
 	if c.rum_limit and not c.rum_limit(mc,e) then return false end
 	return mc:IsType(TYPE_XYZ,c,SUMMON_TYPE_XYZ,tp) and c:GetRank()==rk and mc:IsCanBeXyzMaterial(c,tp) and c:IsC() and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
-		and mc:IsCanBeXyzMaterial(c) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
 end
 function c511001822.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c511001822.filter1(chkc,e,tp) end
