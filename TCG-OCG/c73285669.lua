729494650fc9857be73b45cd5ae3c659b0b62d0e
AndreOliveiraMendes <ao_mendes@hotmail.com>
2017-09-10 22:31:38 -0300

    fix
    wrong id

--- TCG-OCG/c73285669.lua
+++ TCG-OCG/c73285669.lua
@@ -5,11 +5,11 @@ function c73285669.initial_effect(c)
 	aux.AddFusionProcMixN(c,true,true,aux.FilterBoolFunction(Card.IsFusionSetCard,0x19),2)
 	aux.AddContactFusion(c,c73285669.contactfil,c73285669.contactop,c73285669.splimit)
 end
-c3779662.material_setcode=0x19
-function c3779662.contactfil(tp)
+c73285669.material_setcode=0x19
+function c73285669.contactfil(tp)
 	return Duel.GetMatchingGroup(Card.IsAbleToDeckOrExtraAsCost,tp,LOCATION_ONFIELD,0,nil)
 end
-function c3779662.contactop(g,tp)
+function c73285669.contactop(g,tp)
 	Duel.ConfirmCards(1-tp,g)
 	Duel.SendtoDeck(g,nil,2,REASON_COST+REASON_MATERIAL)
 end
