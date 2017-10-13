f5067c1aa549640341a2bc3c6ae6aaee888d1133
edo9300 <edoardo762@gmail.com>
2017-10-13 21:24:13 +0200

    MUSTBEMAT link summon

--- utility/proc_link.lua
+++ utility/proc_link.lua
@@ -25,18 +25,19 @@ function Auxiliary.GetLinkCount(c)
 		return 1+0x10000*c:GetLink()
 	else return 1 end
 end
-function Auxiliary.LCheckRecursive(c,tp,sg,mg,lc,ct,minc,maxc,f,specialchk)
+function Auxiliary.LCheckRecursive(c,tp,sg,mg,mustg,lc,ct,minc,maxc,f,specialchk)
+	if mustg:GetCount()>maxc then return false end
 	sg:AddCard(c)
 	ct=ct+1
-	local res=Auxiliary.LCheckGoal(tp,sg,lc,minc,ct,f,specialchk)
-		or (ct<maxc and mg:IsExists(Auxiliary.LCheckRecursive,1,sg,tp,sg,mg,lc,ct,minc,maxc,f,specialchk))
+	local res=Auxiliary.LCheckGoal(tp,sg,mustg,lc,minc,ct,f,specialchk)
+		or (ct<maxc and mg:IsExists(Auxiliary.LCheckRecursive,1,sg,tp,sg,mg,mustg,lc,ct,minc,maxc,f,specialchk))
 	sg:RemoveCard(c)
 	ct=ct-1
 	return res
 end
-function Auxiliary.LCheckGoal(tp,sg,lc,minc,ct,f,specialchk)
+function Auxiliary.LCheckGoal(tp,sg,mustg,lc,minc,ct,f,specialchk)
 	return ct>=minc and sg:CheckWithSumEqual(Auxiliary.GetLinkCount,lc:GetLink(),ct,ct) 
-		and (not specialchk or specialchk(sg,lc,tp)) and Duel.GetLocationCountFromEx(tp,tp,sg,lc)>0
+		and (not specialchk or specialchk(sg,lc,tp)) and Duel.GetLocationCountFromEx(tp,tp,sg,lc)>0 and sg:Includes(mustg)
 end
 function Auxiliary.LinkCondition(f,minc,maxc,specialchk)
 	return	function(e,c)
@@ -44,29 +45,36 @@ function Auxiliary.LinkCondition(f,minc,maxc,specialchk)
 				if c:IsType(TYPE_PENDULUM) and c:IsFaceup() then return false end
 				local tp=c:GetControler()
 				local mg=Duel.GetMatchingGroup(Auxiliary.LConditionFilter,tp,LOCATION_MZONE,0,nil,f,c,tp)
+				local mustg=mg:Filter(aux.FilterMustbemat,nil,SUMMON_TYPE_LINK,c)
 				local sg=Group.CreateGroup()
-				return mg:IsExists(Auxiliary.LCheckRecursive,1,nil,tp,sg,mg,c,0,minc,maxc,f,specialchk)
+				return mg:IsExists(Auxiliary.LCheckRecursive,1,nil,tp,sg,mg,mustg,c,0,minc,maxc,f,specialchk)
 			end
 end
 function Auxiliary.LinkTarget(f,minc,maxc,specialchk)
 	return	function(e,tp,eg,ep,ev,re,r,rp,chk,c)
 				local mg=Duel.GetMatchingGroup(Auxiliary.LConditionFilter,tp,LOCATION_MZONE,0,nil,f,c,tp)
+				local mustg=mg:Filter(aux.FilterMustbemat,nil,SUMMON_TYPE_LINK,c)
 				local sg=Group.CreateGroup()
 				local cancel=false
+				if mustg:GetCount()>0 then
+					sg:Merge(mustg)
+				end
 				while sg:GetCount()<maxc do
-					local cg=mg:Filter(Auxiliary.LCheckRecursive,sg,tp,sg,mg,c,sg:GetCount(),minc,maxc,f,specialchk)
+					local cg=mg:Filter(Auxiliary.LCheckRecursive,sg,tp,sg,mg,mustg,c,sg:GetCount(),minc,maxc,f,specialchk)
 					if cg:GetCount()==0 then break end
 					if sg:GetCount()>=minc and sg:GetCount()<=maxc and Auxiliary.LCheckGoal(tp,sg,c,minc,sg:GetCount(),f,specialchk) then
-						cancel=true 
-					else 
+						cancel=true
+					else
 						cancel=false
 					end
 					local tc=Group.SelectUnselect(cg,sg,tp,cancel,sg:GetCount()==0 or cancel,1,1)
 					if not tc then break end
-					if not sg:IsContains(tc) then
-						sg:AddCard(tc)
-					else
-						sg:RemoveCard(tc)
+					if mustg:GetCount()==0 or not mustg:IsContains(tc) then
+						if not sg:IsContains(tc) then
+							sg:AddCard(tc)
+						else
+							sg:RemoveCard(tc)
+						end
 					end
 				end
 				if sg:GetCount()>0 then
