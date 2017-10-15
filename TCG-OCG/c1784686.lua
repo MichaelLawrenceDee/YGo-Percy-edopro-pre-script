eaa102b2a2ac43f8abe60efff8695096ebe49390
edo9300 <edoardo762@gmail.com>
2017-10-15 18:07:59 +0200

    Updated the eyes of timaeus

--- TCG-OCG/c1784686.lua
+++ TCG-OCG/c1784686.lua
@@ -20,11 +20,13 @@ function c1784686.initial_effect(c)
 end
 function c1784686.tgfilter(c,e,tp)
 	return c:IsFaceup() and c:IsSetCard(0x10a2) and c:IsCanBeFusionMaterial()
-		and Duel.IsExistingMatchingCard(c1784686.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c:GetCode())
+		and Duel.IsExistingMatchingCard(c1784686.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c)
 		and Duel.GetLocationCountFromEx(tp,tp,c)>0
 end
-function c1784686.spfilter(c,e,tp,code)
-	return aux.IsMaterialListCode(c,code) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+function c1784686.spfilter(c,e,tp,mc)
+	local mustg=aux.GetMustBeMaterialGroup(tp,nil,tp,c,nil,REASON_FUSION)
+	return aux.IsMaterialListCode(c,mc:GetCode()) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
+	and (mustg:GetCount()==0 or (mustg:GetCount()==1 and mustg:IsContains(sc))
 end
 function c1784686.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc==0 then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and c1784686.tgfilter(chkc,e,tp) end
