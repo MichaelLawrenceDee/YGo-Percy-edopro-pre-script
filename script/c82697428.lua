3091a4b60e0ee7b27dd911ee6e51ab916c8c7389
Michael Lawrence C. Dee <michael_lawrence_dee@yahoo.com>
2017-11-14 22:45:00 +0800

    Last Will + EXTRA_TRIBUTE

--- script/c82697428.lua
+++ script/c82697428.lua
@@ -2,13 +2,12 @@
 function c82697428.initial_effect(c)
 	--summon with s/t
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(82697428,0))
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SUMMON_PROC)
-	e1:SetCondition(c82697428.otcon)
-	e1:SetOperation(c82697428.otop)
-	e1:SetValue(SUMMON_TYPE_ADVANCE)
+	e1:SetCode(EFFECT_ADD_EXTRA_TRIBUTE)
+	e1:SetTargetRange(LOCATION_SZONE,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_TRAP))
+	e1:SetValue(POS_FACEUP_ATTACK)
 	c:RegisterEffect(e1)
 	--destroy
 	local e2=Effect.CreateEffect(c)
@@ -24,82 +23,6 @@ function c82697428.initial_effect(c)
 end
 function c82697428.counterfilter(c)
 	return c:IsSetCard(0x8)
-end
-function c82697428.otfilter(c)
-	return c:IsType(TYPE_TRAP) and c:IsReleasable()
-end
-function c82697428.exfilter(c,g,sc)
-	if not c:IsReleasable() or g:IsContains(c) or c:IsHasEffect(EFFECT_EXTRA_RELEASE) then return false end
-	local rele=c:GetCardEffect(EFFECT_EXTRA_RELEASE_SUM)
-	if rele then
-		local remct,ct,flag=rele:GetCountLimit()
-		if remct<=0 then return false end
-	else return false end
-	local sume={c:GetCardEffect(EFFECT_UNRELEASABLE_SUM)}
-	for _,te in ipairs(sume) do
-		if type(te:GetValue())=='function' then
-			if te:GetValue()(te,sc) then return false end
-		else return false end
-	end
-	return true
-end
-function c82697428.val(c,sc,ma)
-	local eff3={c:GetCardEffect(EFFECT_TRIPLE_TRIBUTE)}
-	if ma>=3 then
-		for _,te in ipairs(eff3) do
-			if te:GetValue()(te,sc) then return 0x30001 end
-		end
-	end
-	local eff2={c:GetCardEffect(EFFECT_DOUBLE_TRIBUTE)}
-	for _,te in ipairs(eff2) do
-		if te:GetValue()(te,sc) then return 0x20001 end
-	end
-	return 1
-end
-function c82697428.req(c)
-	return c:IsType(TYPE_TRAP) and c:IsLocation(LOCATION_SZONE)
-end
-function c82697428.unreq(c,tp)
-	return c:IsControler(1-tp) and not c:IsHasEffect(EFFECT_EXTRA_RELEASE) and c:IsHasEffect(EFFECT_EXTRA_RELEASE_SUM)
-end
-function c82697428.rescon(sg,e,tp,mg)
-	local c=e:GetHandler()
-	local mi,ma=c:GetTributeRequirement()
-	if mi<1 then mi=ma end
-	if not sg:IsExists(c82697428.req,1,nil) or not aux.ChkfMMZ(1)(sg,e,tp,mg) 
-		or sg:FilterCount(c82697428.unreq,nil,tp)>1 then return false end
-	local ct=sg:GetCount()
-	return sg:CheckWithSumEqual(c82697428.val,mi,ct,ct,c,ma) or sg:CheckWithSumEqual(c82697428.val,ma,ct,ct,c,ma)
-end
-function c82697428.otcon(e,c,minc)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	local g=Duel.GetTributeGroup(c)
-	local exg=Duel.GetMatchingGroup(c82697428.otfilter,tp,LOCATION_SZONE,0,nil)
-	g:Merge(exg)
-	local opg=Duel.GetMatchingGroup(c82697428.exfilter,tp,0,LOCATION_MZONE,nil,g,c)
-	g:Merge(opg)
-	local mi,ma=c:GetTributeRequirement()
-	if mi<minc then mi=minc end
-	if ma<mi then return false end
-	return ma>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>-ma and aux.SelectUnselectGroup(g,e,tp,1,ma,c82697428.rescon,0)
-end
-function c82697428.otop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.GetTributeGroup(c)
-	local exg=Duel.GetMatchingGroup(c82697428.otfilter,tp,LOCATION_SZONE,0,nil)
-	g:Merge(exg)
-	local opg=Duel.GetMatchingGroup(c82697428.exfilter,tp,0,LOCATION_MZONE,nil,g,c)
-	g:Merge(opg)
-	local mi,ma=c:GetTributeRequirement()
-	if mi<1 then mi=1 end
-	local sg=aux.SelectUnselectGroup(g,e,tp,mi,ma,c82697428.rescon,1,tp,HINTMSG_RELEASE)
-	local remc=sg:Filter(c82697428.unreq,nil,tp):GetFirst()
-	if remc then
-		local rele=remc:GetCardEffect(EFFECT_EXTRA_RELEASE_SUM)
-		rele:Reset()
-	end
-	c:SetMaterial(sg)
-	Duel.Release(sg,REASON_SUMMON+REASON_MATERIAL)
 end
 function c82697428.descost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetCustomActivityCount(82697428,tp,ACTIVITY_SPSUMMON)==0 end
--- script/c94982447.lua
+++ script/c94982447.lua
@@ -2,13 +2,12 @@
 function c94982447.initial_effect(c)
 	--summon with s/t
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(94982447,0))
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SUMMON_PROC)
-	e1:SetCondition(c94982447.otcon)
-	e1:SetOperation(c94982447.otop)
-	e1:SetValue(SUMMON_TYPE_ADVANCE)
+	e1:SetCode(EFFECT_ADD_EXTRA_TRIBUTE)
+	e1:SetTargetRange(LOCATION_SZONE,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_CONTINUOUS))
+	e1:SetValue(POS_FACEUP_ATTACK)
 	c:RegisterEffect(e1)
 	--spsummon
 	local e2=Effect.CreateEffect(c)
@@ -35,86 +34,9 @@ function c94982447.initial_effect(c)
 	e4:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 	c:RegisterEffect(e4)
 end
-function c94982447.otfilter(c)
-	return c:IsType(TYPE_CONTINUOUS) and c:IsReleasable()
-end
-function c94982447.exfilter(c,g,sc)
-	if not c:IsReleasable() or g:IsContains(c) or c:IsHasEffect(EFFECT_EXTRA_RELEASE) then return false end
-	local rele=c:GetCardEffect(EFFECT_EXTRA_RELEASE_SUM)
-	if rele then
-		local remct,ct,flag=rele:GetCountLimit()
-		if remct<=0 then return false end
-	else return false end
-	local sume={c:GetCardEffect(EFFECT_UNRELEASABLE_SUM)}
-	for _,te in ipairs(sume) do
-		if type(te:GetValue())=='function' then
-			if te:GetValue()(te,sc) then return false end
-		else return false end
-	end
-	return true
-end
-function c94982447.val(c,sc,ma)
-	local eff3={c:GetCardEffect(EFFECT_TRIPLE_TRIBUTE)}
-	if ma>=3 then
-		for _,te in ipairs(eff3) do
-			if te:GetValue()(te,sc) then return 0x30001 end
-		end
-	end
-	local eff2={c:GetCardEffect(EFFECT_DOUBLE_TRIBUTE)}
-	for _,te in ipairs(eff2) do
-		if te:GetValue()(te,sc) then return 0x20001 end
-	end
-	return 1
-end
-function c94982447.req(c)
-	return c:IsType(TYPE_CONTINUOUS) and c:IsLocation(LOCATION_SZONE)
-end
-function c94982447.unreq(c,tp)
-	return c:IsControler(1-tp) and not c:IsHasEffect(EFFECT_EXTRA_RELEASE) and c:IsHasEffect(EFFECT_EXTRA_RELEASE_SUM)
-end
-function c94982447.rescon(sg,e,tp,mg)
-	local c=e:GetHandler()
-	local mi,ma=c:GetTributeRequirement()
-	if mi<1 then mi=ma end
-	if not sg:IsExists(c94982447.req,1,nil) or not aux.ChkfMMZ(1)(sg,e,tp,mg) 
-		or sg:FilterCount(c94982447.unreq,nil,tp)>1 then return false end
-	local ct=sg:GetCount()
-	return sg:CheckWithSumEqual(c94982447.val,mi,ct,ct,c,ma) or sg:CheckWithSumEqual(c94982447.val,ma,ct,ct,c,ma)
-end
-function c94982447.otcon(e,c,minc)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	local g=Duel.GetTributeGroup(c)
-	local exg=Duel.GetMatchingGroup(c94982447.otfilter,tp,LOCATION_SZONE,0,nil)
-	g:Merge(exg)
-	local opg=Duel.GetMatchingGroup(c94982447.exfilter,tp,0,LOCATION_MZONE,nil,g,c)
-	g:Merge(opg)
-	local mi,ma=c:GetTributeRequirement()
-	if mi<minc then mi=minc end
-	if ma<mi then return false end
-	return ma>0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>-ma and aux.SelectUnselectGroup(g,e,tp,1,ma,c94982447.rescon,0)
-end
-function c94982447.otop(e,tp,eg,ep,ev,re,r,rp,c)
-	local g=Duel.GetTributeGroup(c)
-	local exg=Duel.GetMatchingGroup(c94982447.otfilter,tp,LOCATION_SZONE,0,nil)
-	g:Merge(exg)
-	local opg=Duel.GetMatchingGroup(c94982447.exfilter,tp,0,LOCATION_MZONE,nil,g,c)
-	g:Merge(opg)
-	local mi,ma=c:GetTributeRequirement()
-	if mi<1 then mi=1 end
-	local sg=aux.SelectUnselectGroup(g,e,tp,mi,ma,c94982447.rescon,1,tp,HINTMSG_RELEASE)
-	local remc=sg:Filter(c94982447.unreq,nil,tp):GetFirst()
-	if remc then
-		local rele=remc:GetCardEffect(EFFECT_EXTRA_RELEASE_SUM)
-		rele:Reset()
-	end
-	c:SetMaterial(sg)
-	Duel.Release(sg,REASON_SUMMON+REASON_MATERIAL)
-end
 function c94982447.spcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsPreviousPosition(POS_FACEUP) and
-		c:IsSummonType(SUMMON_TYPE_ADVANCE)
+	return c:IsPreviousPosition(POS_FACEUP) and c:IsSummonType(SUMMON_TYPE_ADVANCE)
 end
 function c94982447.spfilter(c,e,tp)
 	return c:IsSetCard(0xf9) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
