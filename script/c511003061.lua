097a2dcb71828b2a52dcfc982a1d0dec7bb634a6
MichaelLawrenceDee <michael_lawrence_dee@yahoo.com>
2017-11-17 13:09:49 +0800

    Anime Shooting Star Dragon

---
+++ script/c511003061.lua
@@ -0,0 +1,147 @@
+--シューティング・スター・ドラゴン
+--Shooting Star Dragon
+function c511003061.initial_effect(c)
+	--synchro summon
+	aux.AddSynchroProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_SYNCHRO),1,1,aux.FilterBoolFunction(Card.IsCode,44508094),1,1)
+	c:EnableReviveLimit()
+	--opponent's turn synchro
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_EXTRA)
+	e1:SetCondition(c511003061.syncon)
+	e1:SetOperation(c511003061.synop)
+	c:RegisterEffect(e1)
+	--multi attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(24696097,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(c511003061.mtcon)
+	e2:SetOperation(c511003061.mtop)
+	c:RegisterEffect(e2)
+	--negate  and destroy
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_CHAIN_SOLVING)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetOperation(c511003061.negop)
+	c:RegisterEffect(e3)
+	--banish
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(24696097,2))
+	e4:SetCategory(CATEGORY_REMOVE)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetCondition(c511003061.rmcon)
+	e4:SetCost(c511003061.rmcost)
+	e4:SetTarget(c511003061.rmtg)
+	e4:SetOperation(c511003061.rmop)
+	c:RegisterEffect(e4)
+	--Return
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(24696097,3))
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e5:SetCode(EVENT_PHASE+PHASE_END)
+	e5:SetRange(LOCATION_REMOVED)
+	e5:SetCountLimit(1)
+	e5:SetTarget(c511003061.rettg)
+	e5:SetOperation(c511003061.retop)
+	c:RegisterEffect(e5)
+end
+function c511003061.syncon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp and e:GetHandler():IsSynchroSummonable(nil)
+end
+function c511003061.synop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SynchroSummon(tp,e:GetHandler(),nil)
+end
+function c511003061.mtcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsAbleToEnterBP() and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>=5
+end
+function c511003061.mtop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.ConfirmDecktop(tp,5)
+	local g=Duel.GetDecktopGroup(tp,5)
+	local ct=g:FilterCount(Card.IsType,nil,TYPE_TUNER)
+	Duel.ShuffleDeck(tp)
+	if ct>1 then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetValue(ct-1)
+		c:RegisterEffect(e1)
+	elseif ct==0 then
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+		e2:SetValue(1)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e2)
+	end
+end
+function c511003061.cfilter(c,tp)
+	return c:IsOnField() and c:IsControler(tp)
+end
+function c511003061.negop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	local ex,tg,tc=Duel.GetOperationInfo(ev,CATEGORY_DESTROY)
+	if c:IsStatus(STATUS_BATTLE_DESTROYED) or not g or not ex or not tg or not Duel.IsChainNegatable(ev) or not tg:Includes(g) or tc+tg:FilterCount(c511003061.cfilter,c)-tg:GetCount()~=1 
+		or g:FilterCount(c511003061.cfilter,c,tp)~=1 then return false end
+	local rc=re:GetHandler()
+	if Duel.NegateActivation(ev) and rc:IsRelateToEffect(re) then
+		Duel.Destroy(rc,REASON_EFFECT)
+	end
+end
+function c511003061.rmcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp
+end
+function c511003061.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST+REASON_TEMPORARY)
+end
+function c511003061.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	if c:IsLocation(LOCATION_REMOVED) then
+		Duel.SetTargetCard(e:GetHandler())
+	end
+end
+function c511003061.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		c:RegisterFlagEffect(511003061,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,0)
+	end
+	if Duel.GetAttacker() and Duel.GetAttacker():IsControler(1-tp) and Duel.SelectYesNo(tp,94) then
+		Duel.NegateAttack()
+	else
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+		e1:SetCode(EVENT_FREE_CHAIN)
+		e1:SetRange(LOCATION_REMOVED)
+		e1:SetCountLimit(1)
+		e1:SetCondition(c511003061.discon)
+		e1:SetOperation(c511003061.disop)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
+end
+function c511003061.discon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker() and Duel.GetAttacker():IsControler(1-tp)
+end
+function c511003061.disop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.NegateAttack()
+end
+function c511003061.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():GetFlagEffect(511003061)>0 end
+end
+function c511003061.retop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		Duel.ReturnToField(e:GetHandler())
+	end
+end
